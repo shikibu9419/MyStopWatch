eda6c7f9f828f6ab45eb6ec648053feac92e4ca7
Shikibu <shikibu9419@gmail.com>
2018-06-14 22:54:00 +0900

    first build

---
+++ app/src/main/java/info/shikibu/android/my_stop_watch/MainActivity.kt
@@ -0,0 +1,12 @@
+package info.shikibu.android.my_stop_watch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
